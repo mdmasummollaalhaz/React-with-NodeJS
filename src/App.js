2dafe04baee3fc341104b63ca77ad89683813f6b
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-20 20:27:41 +0600

    fetch from own sercer and middleware, handle cors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:6001/user')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data: {users.length} </h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>Id:{user.id} Name: {user.name} email{user.email}</li>
+        ))}
+      </ul>
     </div>
   );
 }
